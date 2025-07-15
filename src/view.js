49936cfcd004f6e88bc1ec102230a677fa0325ed
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-07-15 11:19:27 +0300

    package: sarawebs/sb-utils updates to 2.0.0

--- src/view.js
+++ src/view.js
@@ -2,7 +2,7 @@ import { addCopyRight } from "@sarawebs/sb-utils";
 
 export class View {
   constructor(appName) {
-    addCopyRight(appName);
+    addCopyRight({title: appName});
     document.querySelector("title").textContent = appName;
     document.getElementById("logo-name").textContent = appName;
   }
