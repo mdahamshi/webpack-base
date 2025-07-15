ea16c830d45205901befee8ea84aa370f2f9a266
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-07-15 11:20:00 +0300

    code: prettier

--- src/view.js
+++ src/view.js
@@ -2,7 +2,7 @@ import { addCopyRight } from "@sarawebs/sb-utils";
 
 export class View {
   constructor(appName) {
-    addCopyRight({title: appName});
+    addCopyRight({ title: appName });
     document.querySelector("title").textContent = appName;
     document.getElementById("logo-name").textContent = appName;
   }
